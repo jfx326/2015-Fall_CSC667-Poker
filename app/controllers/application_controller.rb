0de083145cfa103e259475ee19f2d102dfbfb7b6
Joseph Fernandez <fernandezhj26@gmail.com>
2018-01-09 22:47:33 -0800

    Revise configre_permitter_parameters in application_controller.rb according to updated Devise API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,16 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me)}
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password, :image)}
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permi  t(:first_name, :last_name, :username, :email, :password, :password_confirmation, :remember_me)
+    end
+    
+    devise_parameter_sanitizer.permit(:sign_in) do |user|
+      user.permit(:login, :username, :email, :password, :remember_me)
+    end
+    
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password, :image)
+    end
   end
 end
